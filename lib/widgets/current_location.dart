1d16822578216350f972bf011e27465180e9ffe5
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-10-17 00:57:54 +0700

    refactor: new logic of fetching and showing location

--- lib/widgets/current_location.dart
+++ lib/widgets/current_location.dart
@@ -1,64 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:sky_watch/services/location.dart';
-import 'package:sky_watch/services/openweather.dart';
+import 'package:sky_watch/services/instances.dart'; // Your weather service
 
-class CurrentLocation extends StatefulWidget {
-  const CurrentLocation({super.key});
+class CurrentLocation extends StatelessWidget {
+  final double latitude;
+  final double longitude;
 
-  @override
-  State<CurrentLocation> createState() => _CurrentLocationState();
-}
+  const CurrentLocation(
+      {super.key, required this.latitude, required this.longitude});
 
-class _CurrentLocationState extends State<CurrentLocation> {
   @override
   Widget build(BuildContext context) {
-    const TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0);
-    return FutureBuilder(
-        future: SkyWatchGeolocator().getCurrentCityLocation(),
-        builder: (BuildContext context, AsyncSnapshot<Position?> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          }
-          if (snapshot.hasError) {
-            return const Text(
-              'Error while retrieving location info. Please allow the permission',
-              style: TextStyle(color: Colors.redAccent),
-            );
-          }
-          if (snapshot.hasData) {
-            Position? position = snapshot.data;
-            var latitude = position?.latitude;
-            var longitude = position?.longitude;
-            final openWeatherApiKey = dotenv.env['OPENWEATHER_API_KEY'];
+    return FutureBuilder<String?>(
+      future: swWeather.fetchCurrentCityNameByCoord(
+          latitude: latitude, longitude: longitude),
+      builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text(
+              'Fetching city information...'); // Show loading while waiting
+        }
 
-            return FutureBuilder(
-              builder: (BuildContext context,
-                  AsyncSnapshot<dynamic> weatherSnapshot) {
-                if (weatherSnapshot.connectionState ==
-                    ConnectionState.waiting) {
-                  return const CircularProgressIndicator(); // Show loading while waiting for API response
-                }
-                if (weatherSnapshot.hasError) {
-                  return const Text(
-                      'Error while retrieving city information. Please contact the app publisher');
-                }
-                if (weatherSnapshot.hasData) {
-                  return Text(
-                      weatherSnapshot.data); // Display the API response data
-                }
-                return const Text('Failed to retrieve weather data');
-              },
-              future: SkyWatchWeather(
-                      apiKey: openWeatherApiKey!,
-                      latitude: latitude!,
-                      longitude: longitude!)
-                  .fetchCityNameByCoordinate(),
-            );
-          }
+        if (snapshot.hasError) {
+          return const Text('Error while retrieving city information.');
+        }
 
-          return const Text('Failed to retrieve location data');
-        });
+        if (snapshot.hasData) {
+          return Text(
+              snapshot.data ?? 'Unknown Location'); // Display the city name
+        }
+
+        return const Text('Failed to retrieve city information.');
+      },
+    );
   }
 }
