c904770e4cf424852d717388b1f1efb49e953176
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-10-17 00:56:20 +0700

    refactor: implement centralized instances

--- lib/widgets/current_date_day.dart
+++ lib/widgets/current_date_day.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:sky_watch/services/date.dart';
+import 'package:sky_watch/services/instances.dart';
 
 class CurrentDateAndDay extends StatelessWidget {
   const CurrentDateAndDay({super.key});
   @override
   Widget build(BuildContext context) {
-    return Text(SkyWatchDateTime().getDateAndDay());
+    return Text(swDt.getDateAndDay());
   }
 }
