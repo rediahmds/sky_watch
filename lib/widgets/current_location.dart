51c26adbd60f19ee9d5a89defcc9f6c0d3ea325f
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-10-22 15:11:37 +0700

    ui: centerize current location data

--- lib/widgets/current_location.dart
+++ lib/widgets/current_location.dart
@@ -4,9 +4,13 @@ import 'package:sky_watch/services/instances.dart'; // Your weather service
 class CurrentLocation extends StatelessWidget {
   final double latitude;
   final double longitude;
+  final TextStyle style;
 
   const CurrentLocation(
-      {super.key, required this.latitude, required this.longitude});
+      {super.key,
+      required this.latitude,
+      required this.longitude,
+      this.style = const TextStyle(fontSize: 30.0)});
 
   @override
   Widget build(BuildContext context) {
@@ -15,20 +19,29 @@ class CurrentLocation extends StatelessWidget {
           latitude: latitude, longitude: longitude),
       builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Text(
-              'Fetching city information...'); // Show loading while waiting
+          return Text(
+            'Fetching city information...',
+            style: style,
+          ); // Show loading while waiting
         }
 
         if (snapshot.hasError) {
-          return const Text('Error while retrieving city information.');
+          return Text('Error while retrieving city information.', style: style);
         }
 
         if (snapshot.hasData) {
-          return Text(
-              snapshot.data ?? 'Unknown Location'); // Display the city name
+          return Center(
+            child: Text(
+              snapshot.data ?? 'Unknown Location',
+              style: style,
+            ),
+          ); // Display the city name
         }
 
-        return const Text('Failed to retrieve city information.');
+        return Text(
+          'Failed to retrieve city information.',
+          style: style,
+        );
       },
     );
   }
