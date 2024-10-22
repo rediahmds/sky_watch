48317ece62479df302d044a4e8b5476dca2be786
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-10-22 15:11:07 +0700

    ui: restyle date and day widget
    added rounded background

--- lib/widgets/current_date_day.dart
+++ lib/widgets/current_date_day.dart
@@ -3,8 +3,21 @@ import 'package:sky_watch/services/instances.dart';
 
 class CurrentDateAndDay extends StatelessWidget {
   const CurrentDateAndDay({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Text(swDt.getDateAndDay());
+    return Center(
+        child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 15.0),
+            padding:
+                const EdgeInsets.symmetric(vertical: 4.0, horizontal: 25.0),
+            decoration: BoxDecoration(
+              color: Colors.black87,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Text(
+              swDt.getDateAndDay(),
+              style: const TextStyle(color: Color(0xffffe142)),
+            )));
   }
 }
