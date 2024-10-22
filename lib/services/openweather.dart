498c443a8a7b1f68283ec949a0f17a23577682f5
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-10-22 15:09:58 +0700

    feat: fetch five days forecast

--- lib/services/openweather.dart
+++ lib/services/openweather.dart
@@ -26,11 +26,50 @@ class SkyWatchWeather {
 
   Future<Weather> fetchCurrentWeatherByCoord(
       {required double latitude, required double longitude}) async {
-    final currentWeather = wf.currentWeatherByLocation(latitude, longitude);
-    return currentWeather;
+    try {
+      final currentWeather = wf.currentWeatherByLocation(latitude, longitude);
+      return currentWeather;
+    } on OpenWeatherAPIException catch (e) {
+      logger.e(e);
+      rethrow;
+    }
   }
 
-  String fetchWeatherIconURL(String weatherIconCode) {
-    return 'https://openweathermap.org/img/wn/$weatherIconCode@4x.png';
+  String fetchWeatherIconURL({required String weatherIconCode, int size = 4}) {
+    return 'https://openweathermap.org/img/wn/$weatherIconCode@${size}x.png';
+  }
+
+  List<Weather> _filterToFiveDays(List<Weather> weatherData) {
+    Set<String> uniqueDt = {};
+    final List<Weather> fiveDaysWeather = [];
+    for (var weather in weatherData) {
+      final String key =
+          '${weather.date!.year}-${weather.date!.month}-${weather.date!.day}';
+
+      if (uniqueDt.length < 5 && !uniqueDt.contains(key)) {
+        uniqueDt.add(key);
+        fiveDaysWeather.add(weather);
+      }
+    }
+    return fiveDaysWeather;
+  }
+
+  Future<List<Weather>> fetchWeatherForecastByCoord(
+      {required double latitude,
+      required double longitude,
+      bool fiveDays = false}) async {
+    try {
+      final result = await wf.fiveDayForecastByLocation(latitude, longitude);
+
+      if (fiveDays) {
+        final List<Weather> fiveDaysWeather = _filterToFiveDays(result);
+        return fiveDaysWeather;
+      }
+
+      return result;
+    } on OpenWeatherAPIException catch (e) {
+      logger.e(e);
+      rethrow;
+    }
   }
 }
