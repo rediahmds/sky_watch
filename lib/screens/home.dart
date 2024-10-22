7dce3dab764aa04b720dc0a2f2e8e34d6cf24637
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-10-23 01:41:41 +0700

    refactor: remove unecessary package

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:hugeicons/hugeicons.dart';
 import 'package:sky_watch/services/instances.dart';
