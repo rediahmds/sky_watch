fd5c0ce2ac27a9d8abb6f2b4fd46b66374850309
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-10-22 15:08:23 +0700

    feat: five days forecast
    and several refactoring here and there

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,6 +4,7 @@ import 'package:sky_watch/services/instances.dart';
 import 'package:sky_watch/widgets/current_date_day.dart';
 import 'package:sky_watch/widgets/current_location.dart';
 import 'package:sky_watch/widgets/current_weather_info.dart';
+import 'package:sky_watch/widgets/five_days_forecast.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -25,49 +26,50 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xffffe142),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Center(
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              child: FutureBuilder<Position>(
-                future: _position, // Use FutureBuilder to wait for location
-                builder:
-                    (BuildContext context, AsyncSnapshot<Position> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const CircularProgressIndicator(); // Show loading indicator
-                  }
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: FutureBuilder<Position>(
+              future: _position,
+              builder:
+                  (BuildContext context, AsyncSnapshot<Position> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator(); // Show loading indicator
+                }
 
-                  if (snapshot.hasError) {
-                    return Center(
-                        child: Text(
-                            'Error: ${snapshot.error}')); // Show error message
-                  }
+                if (snapshot.hasError) {
+                  return Center(
+                      child: Text(
+                          'Error: ${snapshot.error}')); // Show error message
+                }
 
-                  if (snapshot.hasData) {
-                    final position =
-                        snapshot.data!; // Extract the Position data
+                if (snapshot.hasData) {
+                  final position = snapshot.data!; // Extract the Position data
 
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        const CurrentDateAndDay(),
-                        CurrentLocation(
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      CurrentLocation(
+                        latitude: position.latitude,
+                        longitude: position.longitude,
+                      ),
+                      const CurrentDateAndDay(),
+                      CurrentWeatherInfo(
+                        latitude: position.latitude,
+                        longitude: position.longitude,
+                      ),
+                      FiveDaysForecast(
                           latitude: position.latitude,
-                          longitude: position.longitude,
-                        ),
-                        CurrentWeatherInfo(
-                          latitude: position.latitude,
-                          longitude: position.longitude,
-                        ),
-                      ],
-                    );
-                  }
+                          longitude: position.longitude)
+                    ],
+                  );
+                }
 
-                  return const Text('Failed to retrieve location data.');
-                },
-              ),
+                return const Text('Failed to retrieve location data.');
+              },
             ),
           ),
         ),
