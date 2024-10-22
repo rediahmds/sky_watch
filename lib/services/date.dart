e90344e78deb1633849d292adff2095b2946f708
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-10-22 15:09:04 +0700

    utils: methods to format date time data

--- lib/services/date.dart
+++ lib/services/date.dart
@@ -5,4 +5,8 @@ class SkyWatchDateTime {
     DateTime now = DateTime.now();
     return DateFormat('d MMM, EEEE').format(now);
   }
+
+  String showDateMonthOnly(DateTime date) => DateFormat('d MMM').format(date);
+
+  String showTimeOnly(DateTime date) => DateFormat('HH:mm').format(date);
 }
