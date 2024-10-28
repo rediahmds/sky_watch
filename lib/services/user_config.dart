6b72eb9b8e4cf9a9c8dc0d7eb278a071581aac61
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-10-28 15:28:33 +0700

    feat: factory class to store user config from settings page

--- lib/services/user_config.dart
+++ lib/services/user_config.dart
@@ -2,6 +2,10 @@ class UserConfig {
   static final UserConfig _instance = UserConfig._internal();
   String? apiKey;
   bool? isTextFieldEnabled;
+  bool? isCustomCoordEnabled;
+
+  double? latitude;
+  double? longitude;
 
   factory UserConfig() {
     return _instance;
