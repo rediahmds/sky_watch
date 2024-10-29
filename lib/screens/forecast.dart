1ebbb13bb54a01bf1d6c83ce6f3d4a28f30959dc
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-10-30 00:41:13 +0700

    ui: web responsive
    also, font size scales automatically

--- lib/screens/forecast.dart
+++ lib/screens/forecast.dart
@@ -14,139 +14,188 @@ class Forecast extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: primaryColor,
-        appBar: AppBar(
-          title: const Text('Forecast'),
-          centerTitle: true,
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: const Icon(Icons.chevron_left_rounded)),
-          backgroundColor: primaryColor,
+      backgroundColor: primaryColor,
+      appBar: AppBar(
+        title: const Text('Forecast'),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.chevron_left_rounded),
         ),
-        body: SafeArea(
-            child: FutureBuilder<Position>(
-                future: position,
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
+        backgroundColor: primaryColor,
+      ),
+      body: SafeArea(
+        child: FutureBuilder<Position>(
+          future: position,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
 
-                  if (snapshot.hasData) {
-                    final latitude = snapshot.data!.latitude;
-                    final longitude = snapshot.data!.longitude;
+            if (snapshot.hasData) {
+              final latitude = snapshot.data!.latitude;
+              final longitude = snapshot.data!.longitude;
 
-                    return ValueListenableBuilder<String?>(
-                        valueListenable: openWeatherApiKeyNotifier,
-                        builder: (context, value, child) {
-                          return FutureBuilder<List<Weather>>(
-                              future: swWeather.fetchWeatherForecastByCoord(
-                                  latitude: latitude, longitude: longitude),
-                              builder: (BuildContext context,
-                                  AsyncSnapshot<List<Weather>>
-                                      forecastSnapshot) {
-                                if (forecastSnapshot.connectionState ==
-                                    ConnectionState.waiting) {
-                                  return const Center(
-                                    child: CircularProgressIndicator(),
-                                  );
-                                }
+              return ValueListenableBuilder<String?>(
+                valueListenable: openWeatherApiKeyNotifier,
+                builder: (context, value, child) {
+                  return FutureBuilder<List<Weather>>(
+                    future: swWeather.fetchWeatherForecastByCoord(
+                        latitude: latitude, longitude: longitude),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<Weather>> forecastSnapshot) {
+                      if (forecastSnapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
 
-                                if (forecastSnapshot.hasData) {
-                                  final forecast = forecastSnapshot.data!;
+                      if (forecastSnapshot.hasData) {
+                        final forecast = forecastSnapshot.data!;
 
-                                  return ListView.builder(
-                                    itemBuilder:
-                                        (BuildContext context, int index) {
-                                      final weather = forecast[index];
-                                      final weatherIcon = weather.weatherIcon!;
-                                      final iconUrl =
-                                          swWeather.fetchWeatherIconURL(
-                                              weatherIconCode: weatherIcon,
-                                              size: 2);
-                                      final date = weather.date!;
-                                      final temp = NumberFormat('#')
-                                          .format(weather.temperature!.celsius);
+                        return LayoutBuilder(
+                          builder: (context, constraints) {
+                            final isLargeScreen = constraints.maxWidth > 600;
 
-                                      return Card(
-                                        margin: const EdgeInsets.symmetric(
-                                            vertical: 6.0, horizontal: 14.0),
-                                        color: primaryColor,
-                                        elevation: 0,
-                                        shape: RoundedRectangleBorder(
-                                            side: const BorderSide(width: 4.0),
-                                            borderRadius:
-                                                BorderRadius.circular(20.0)),
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(8.0),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceEvenly,
-                                            children: [
-                                              Column(
-                                                children: [
-                                                  Text(
-                                                    swDt.getDayName(date),
-                                                    style: const TextStyle(
-                                                        fontWeight:
-                                                            FontWeight.w700),
-                                                  ),
-                                                  Text(swDt
-                                                      .getMonthAndDate(date))
-                                                ],
-                                              ),
-                                              Column(
-                                                children: [
-                                                  Text(
-                                                    '$temp°C',
-                                                    style: const TextStyle(
-                                                        fontSize: 30.0,
-                                                        fontWeight:
-                                                            FontWeight.w700),
-                                                  ),
-                                                  Text(swDt
-                                                      .getTimeIn12HourFormat(
-                                                          date)),
-                                                ],
-                                              ),
-                                              Column(
-                                                children: [
-                                                  Image.network(iconUrl),
-                                                  Text(weather.weatherMain!)
-                                                ],
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                    itemCount: forecast.length,
-                                  );
-                                }
+                            return Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: isLargeScreen
+                                  ? GridView.builder(
+                                      gridDelegate:
+                                          const SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 2,
+                                        childAspectRatio: 2.5,
+                                        crossAxisSpacing: 10,
+                                        mainAxisSpacing: 10,
+                                      ),
+                                      itemCount: forecast.length,
+                                      itemBuilder:
+                                          (BuildContext context, int index) {
+                                        return _buildWeatherCard(
+                                            context, forecast[index]);
+                                      },
+                                    )
+                                  : ListView.builder(
+                                      itemCount: forecast.length,
+                                      itemBuilder:
+                                          (BuildContext context, int index) {
+                                        return _buildWeatherCard(
+                                            context, forecast[index]);
+                                      },
+                                    ),
+                            );
+                          },
+                        );
+                      }
 
-                                return Center(
-                                  child: Column(
-                                    children: [
-                                      const Text(
-                                          'Failed to retrieve forecast data.'),
-                                      Text(forecastSnapshot.error.toString())
-                                    ],
-                                  ),
-                                );
-                              });
-                        });
-                  }
+                      return Center(
+                        child: Column(
+                          children: [
+                            const Text('Failed to retrieve forecast data.'),
+                            Text(forecastSnapshot.error.toString()),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+              );
+            }
 
-                  return Center(
-                      child: Column(
+            return Center(
+              child: Column(
+                children: [
+                  const Text('Failed to retrieve data.'),
+                  Text(snapshot.error.toString()),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWeatherCard(BuildContext context, Weather weather) {
+    final weatherIcon = weather.weatherIcon!;
+    final iconUrl =
+        swWeather.fetchWeatherIconURL(weatherIconCode: weatherIcon, size: 2);
+    final date = weather.date!;
+    final temp = NumberFormat('#').format(weather.temperature!.celsius);
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        // Adjust text sizes based on available width in constraints
+        double titleFontSize = constraints.maxWidth < 400 ? 14 : 16;
+        double dateFontSize = constraints.maxWidth < 400 ? 12 : 14;
+        double tempFontSize = constraints.maxWidth < 400 ? 24 : 28;
+        double mainFontSize = constraints.maxWidth < 400 ? 12 : 14;
+        double iconScale = constraints.maxWidth < 400 ? 1.5 : 1.0;
+
+        return Card(
+          margin: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 14.0),
+          color: primaryColor,
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(width: 4.0),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Text('Failed to retrieve data.'),
-                      Text(snapshot.error.toString()),
+                      Text(
+                        swDt.getDayName(date),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: titleFontSize,
+                        ),
+                      ),
+                      Text(
+                        swDt.getMonthAndDate(date),
+                        style: TextStyle(fontSize: dateFontSize),
+                      ),
                     ],
-                  ));
-                })));
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '$temp°C',
+                        style: TextStyle(
+                          fontSize: tempFontSize,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        swDt.getTimeIn12HourFormat(date),
+                        style: TextStyle(fontSize: dateFontSize),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(iconUrl, scale: iconScale),
+                      Text(
+                        weather.weatherMain!,
+                        style: TextStyle(fontSize: mainFontSize),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
